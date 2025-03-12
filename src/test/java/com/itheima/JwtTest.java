d0cd261035730c0c915a760054163e2819e458eb
wang <2785449886@qq.com>
2025-03-12 20:16:23 +0800

    “大事件”web应用后端开发-打包

--- src/test/java/com/itheima/JwtTest.java
+++ src/test/java/com/itheima/JwtTest.java
@@ -26,17 +26,5 @@ public class JwtTest {
 
         System.out.println(token);
     }
-    @Test
-    public  void testParse(){
-        //定义字符串，模拟用户传递过来的token
-        String token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoxLCJ1c2VybmFtZSI6IuW8oOS4iSJ9LCJleHAiOjE3NDEyNTI0MjR9.0Fe2c-dQoF4Fi4_tub3MP40x-P7q1MurDH1_Z43RrKk";
-        JWTVerifier jwtVerifier = JWT.require(Algorithm.HMAC256("itheima")).build();
 
-        DecodedJWT decodedJWT = jwtVerifier.verify(token);//验证token,生成一个解析后的JWT对象
-        Map<String, Claim> claims =  decodedJWT.getClaims();
-        System.out.println(claims.get("user"));
-        //如果篡改了头部和载荷部分的数据，验证失败
-        // 如果篡改了密钥，也会验证失败
-        //token过期也会失败
-    }
 }
